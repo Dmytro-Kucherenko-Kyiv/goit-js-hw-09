555ed0a926dc6098e69f3eeedd783aff044d2c60
Dmytro Kucherenko <110055719+Dmytro-Kucherenko-Kyiv@users.noreply.github.com>
2023-03-05 23:22:10 +0200

    task 1 v0.1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,16 @@
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
 
+const changeColor = document.querySelector('[data-start]');
+const changeColorStop = document.querySelector('[data-stop]');
+const body = document.querySelector("body");
+
+changeColor.addEventListener('click', (event) => {
+  const randomColor = getRandomHexColor();
+  body.style.backgroundColor = randomColor;
+})
+
+changeColorStop.addEventListener('click', (event) => {
+
+})
