b27f764f9197c5d03f5387e7383144d83e9a67af
Dmytro-Kucherenko-Kyiv <110055719+Dmytro-Kucherenko-Kyiv@users.noreply.github.com>
2023-03-06 20:33:24 +0000

    Deploying to gh-pages from @ Dmytro-Kucherenko-Kyiv/goit-js-hw-09@f1407df4d260ec18bd8486fd481e0052ca0c259b 🚀

--- 01-color-switcher.46b97902.js
+++
@@ -1,2 +0,0 @@
-!function(){var t=document.querySelector("[data-start]"),e=document.querySelector("[data-stop]"),n=document.querySelector("body");t.addEventListener("click",(function(){timerId=setInterval((function(){n.style.backgroundColor="#".concat(Math.floor(16777215*Math.random()).toString(16))}),1e3)})),e.addEventListener("click",(function(){clearInterval(timerId)}))}();
-//# sourceMappingURL=01-color-switcher.46b97902.js.map
