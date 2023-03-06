418960c80c29bb06f75ea9f1d7a99d7fbd1cba91
Dmytro-Kucherenko-Kyiv <110055719+Dmytro-Kucherenko-Kyiv@users.noreply.github.com>
2023-03-06 20:43:47 +0000

    Deploying to gh-pages from @ Dmytro-Kucherenko-Kyiv/goit-js-hw-09@8368927ce2f6b41db5f3e8857fc8b053f732cf2e 🚀

---
+++ 01-color-switcher.843556ea.js
@@ -0,0 +1,2 @@
+const e=document.querySelector("[data-start]"),t=document.querySelector("[data-stop]"),d=document.querySelector("body");e.addEventListener("click",(()=>{timer=setInterval((()=>{d.style.backgroundColor=`#${Math.floor(16777215*Math.random()).toString(16)}`,e.disabled=!0,t.disabled=!1}),1e3)})),t.addEventListener("click",(()=>{clearInterval(timer),e.disabled=!1,t.disabled=!0}));
+//# sourceMappingURL=01-color-switcher.843556ea.js.map
